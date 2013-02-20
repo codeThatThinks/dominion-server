7010f6c9c27b51d7780e0c448573b1a0b035cf58
Ian Glen <codeThatThinks@gmail.com>
2013-02-19 23:26:29 -0500

    Express 3.x compatibility

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express();
-var io = require('socket.io').listen(app);
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 app.listen(80);
 
@@ -14,7 +14,7 @@ io.sockets.on('connection', function(socket)
 	socket.on('set country', function(name, color, callback)
 	{
 		console.log("New country:" . name);
-		
+
 		socket.set('country', name);
 		socket.set('color', color, function()
 		{
