3df2bebe8e44b8b3564d5cd4105c8f30156718a5
Ian Glen <codeThatThinks@gmail.com>
2013-02-19 23:16:58 -0500

    Initial multiplayer code

---
+++ server.js
@@ -0,0 +1,21 @@
+var app = require('express').createServer();
+var io = require('socket.io').listen(app);
+
+app.listen(80);
+
+app.get('/', function (req, res)
+{
+	res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket)
+{
+	socket.on('set country', function(name, color, callback)
+	{
+		socket.set('country', name);
+		socket.set('color', color, function()
+		{
+			callback('success');
+		});
+	});
+});
