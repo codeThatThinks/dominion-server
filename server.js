f2113e4563c3749c681a38e7c1653e9e8ddaf6f2
Ian Glen <codeThatThinks@gmail.com>
2013-02-21 18:51:45 -0500

    Added tracking and relaying of country and territory data

--- server.js
+++ server.js
@@ -2,18 +2,75 @@ var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
+/* variables */
+var territory = new Array();
+
+/* objects */
+function Point(x, y)
+{
+	function set(newX, newY)
+	{
+		this.x = newX;
+		this.y = newY;
+	}
+
+	this.x = x;
+	this.y = y;
+	this.set = set;
+}
+
+function territoryUnit(point, country)
+{
+	this.point = point;
+	this.country = country;
+}
+
 server.listen(80);
 
 io.sockets.on('connection', function(socket)
 {
-	socket.on('set country', function(name, color, callback)
+	socket.on('connect', function(country, color, callback)
 	{
-		console.log("New country:" + name);
-
-		socket.set('country', name);
-		socket.set('color', color, function()
+		socket.set('country', country, function()
 		{
-			callback('success');
+			socket.set('color', color, function()
+			{
+				socket.broadcast.emit('addCountry', socket.get('country'), socket.get('color'));
+				callback('success');
+			});
 		});
+	});
+
+	socket.on('disconnect', function()
+	{
+		// unclaim all territory for that country
+		for(var n = 0; n < territory.length; n++)
+		{
+			if(territory[n].country == socket.get('country'))
+			{
+				territory.splice(n, 1);
+				unclaim(territory[n].point.x, territory[n].point.y, socket.get('country'));
+			}
+		}
+
+		socket.broadcast.emit('removeCountry', socket.get('country'));
+	});
+
+	socket.on('claim', function(x, y)
+	{
+		territory.push(new territoryUnit(new Point(x,y), socket.get('country')));
+		socket.broadcast.emit('claim', x, y, socket.get('country'));
+	});
+
+	socket.on('unclaim', function(x, y)
+	{
+		for(var n = 0; n < territory.length; n++)
+		{
+			if(territory[n].country == socket.get('country'))
+			{
+				territory.splice(n, 1);
+				unclaim(territory[n].point.x, territory[n].point.y, socket.get('country'));
+			}
+		}
 	});
 });
